9bd9c925ff4260106daa7bbf6dd05f9fb814b3a1
Matthew Klein <matthew-a-klein@github.com>
2022-10-02 13:28:49 +0100

    Complete Solution

--- primes.py
+++ primes.py
@@ -1,6 +1,26 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def divisible_by_any(number, list):
+    return any(map(lambda y: number%y == 0, list))
+
+
 def primes(number_of_primes):
-    list = []
-    return list
+    if number_of_primes <=0:
+        raise ValueError
+    else:
+        prime_list = []
+        prime_list.append(2)
+
+        primes_left = number_of_primes - 1
+        number = prime_list[-1] + 1
+        while primes_left > 0:
+
+            if divisible_by_any(number, prime_list):
+                number +=1
+            else:
+                prime_list.append(number)
+                primes_left-=1
+
+
+    return prime_list
